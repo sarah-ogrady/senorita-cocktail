6105836b533710f5245a7877deaf9e4eff9a9c2c
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-05-15 09:06:49 +0100

    deleted line in webpacker as was repeated

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,7 +2,7 @@ const { environment } = require('@rails/webpacker')
 
 module.exports = environment
 
-const { environment } = require('@rails/webpacker')
+
 
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',

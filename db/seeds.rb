c1b41451e57f1ef90686f264c5796106f483c7dd
Sarah OGrady <sarahfrancesogrady@gmail.com>
2020-05-15 11:01:24 +0100

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,16 +12,53 @@ celery_juice = Ingredient.create(name: "celery juice")
 puts "Finish Ingredients"
 
 puts "Create Cocktails"
-bloody_mary = Cocktail.create(name: "Bloody Mary")
-Cocktail.create(name: "Mojito")
-Cocktail.create(name: "Dark and Stormy")
-Cocktail.create(name: "Vodka Lime")
-Cocktail.create(name: "Cherry Red")
-Cocktail.create(name: "Strawberry Daquiri")
-Cocktail.create(name: "Cuba Libre")
-Cocktail.create(name: "Negroni")
-Cocktail.create(name: "Aperol Spritz")
-puts "finish cocktails"
+
+file = URI.open('https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2018/12/212.jpg')
+bloody_mary = Cocktail.new(name: "Bloody Mary")
+bloody_mary.photo.attach(io: file, filename: 'mary.jpg', content_type: 'image/jpg')
+bloody_mary.save
+
+file = URI.open('https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2018/12/212.jpg')
+mojito = Cocktail.new(name: "Mojito")
+mojito.photo.attach(io: file, filename: 'mojito.jpg', content_type: 'image/jpg')
+mojito.save
+
+file = URI.open('https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2018/12/212.jpg')
+dark_and_stormy = Cocktail.new(name: "Dark and Stormy")
+dark_and_stormy.photo.attach(io: file, filename: 'dark_and_stormy.jpg', content_type: 'image/jpg')
+dark_and_stormy.save
+
+file = URI.open('https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2018/12/212.jpg')
+vodka_lime = Cocktail.new(name: "Vodka Lime")
+vodka_lime.photo.attach(io: file, filename: 'vodka_lime.jpg', content_type: 'image/jpg')
+vodka_lime.save
+
+file = URI.open('https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2018/12/212.jpg')
+cherry_red = Cocktail.new(name: "Cherry Red")
+cherry_red.photo.attach(io: file, filename: 'cherry_red.jpg', content_type: 'image/jpg')
+cherry_red.save
+
+file = URI.open('https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2018/12/212.jpg')
+strawberry_daquiri = Cocktail.new(name: "Strawberry Daquiri")
+strawberry_daquiri.photo.attach(io: file, filename: 'strawberry_daquiri.jpg', content_type: 'image/jpg')
+strawberry_daquiri.save
+
+file = URI.open('https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2018/12/212.jpg')
+cuba_libre = Cocktail.new(name: "Cuba Libre")
+cuba_libre.photo.attach(io: file, filename: 'cuba_libre.jpg', content_type: 'image/jpg')
+cuba_libre.save
+
+file = URI.open('https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2018/12/212.jpg')
+negroni = Cocktail.new(name: "Negroni")
+negroni.photo.attach(io: file, filename: 'negroni.jpg', content_type: 'image/jpg')
+negroni.save
+
+file = URI.open('https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2018/12/212.jpg')
+aperol_spritz = Cocktail.new(name: "Aperol Spritz")
+aperol_spritz.photo.attach(io: file, filename: 'aperol_spritz.jpg', content_type: 'image/jpg')
+aperol_spritz.save
+
+puts "Finished cocktails"
 
 puts "Create Doses"
 # dose_uno = Dose.create(cocktail_id: bloody_mary.id, ingredient_id: celery_juice.id, description: "blah blah" )
